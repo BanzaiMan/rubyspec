e03ab0fe6fc9ade398d46afa6a4ca4fdc0d3e75f
Hiro Asari <asari.ruby@gmail.com>
2010-07-14 16:41:59 -0400

    Skeleton for WIN32OLE_EVENT specs.
    MRI trunk uses ADO for tests, but apparently that doesn't work with 1.8.

---
+++ library/win32ole/win32ole_event/message_loop_spec.rb
@@ -0,0 +1,9 @@
+platform_is :windows do
+  require 'win32ole'
+  
+  # not sure how we should test this
+  describe 'WIN32OLE_EVENT.message_loop' do
+    
+  end
+
+end
---
+++ library/win32ole/win32ole_event/new_spec.rb
@@ -0,0 +1,24 @@
+platform_is :windows do
+  require 'win32ole'
+  
+  describe 'WIN32OLE_EVENT.new' do
+    before :each do
+      @ole = WIN32OLE.new('InternetExplorer.Application')
+    end
+    
+    after :each do
+      @ole = nil
+    end
+  
+    it 'raises TypeError given invalid argument' do
+      lambda { WIN32OLE_EVENT.new "A" }.should raise_error TypeError
+    end
+    
+    it 'creates WIN32OLE_EVENT object' do
+      ev = WIN32OLE_EVENT.new(@ole, 'DWebBrowserEvents')
+      ev.should be_kind_of WIN32OLE_EVENT
+    end
+    
+  end
+  
+end
