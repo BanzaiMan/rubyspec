52a08721156bd1c863f2657100be1528fd6d8868
Hiro Asari <asari.ruby@gmail.com>
2010-07-14 14:58:51 -0400

    Actually test if WIN32OLE.const_load defines a constant in expected place.

--- library/win32ole/win32ole/const_load_spec.rb
+++ library/win32ole/win32ole/const_load_spec.rb
@@ -1,26 +1,31 @@
 platform_is :windows do
   require 'win32ole'
   
-  describe 'WIN32OLE.const_load with one argument' do
+  describe 'WIN32OLE.const_load when passed Shell.Application OLE object' do
     before :each do
       @win32ole = WIN32OLE.new 'Shell.Application'
     end
     
-    it 'loads constants into WIN32OLE namespace' do
+    it 'loads constant SsfWINDOWS into WIN32OLE namespace' do
+      WIN32OLE.const_defined?(:SsfWINDOWS).should be_false
       WIN32OLE.const_load @win32ole
-      WIN32OLE.constants.should_not be_empty
+      WIN32OLE.const_defined?(:SsfWINDOWS).should be_true
     end
   end
   
-  describe 'WIN32OLE.const_load with two arguments' do
+  describe 'WIN32OLE.const_load when namespace is specified' do
     before :each do
       module WIN32OLE_RUBYSPEC; end
       @win32ole = WIN32OLE.new 'Shell.Application'
     end
     
     it 'loads constants into given namespace' do
+      module WIN32OLE_RUBYSPEC; end
+      
+      WIN32OLE_RUBYSPEC.const_defined?(:SsfWINDOWS).should be_false
       WIN32OLE.const_load @win32ole, WIN32OLE_RUBYSPEC
-      WIN32OLE_RUBYSPEC.constants.should_not be_empty
+      WIN32OLE_RUBYSPEC.const_defined?(:SsfWINDOWS).should be_true
+      
     end
   end
 
