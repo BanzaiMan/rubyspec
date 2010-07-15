60b8811d80f38b557c8f4a99b81ce83de97049cc
Hiro Asari <asari.ruby@gmail.com>
2010-07-15 11:08:05 -0400

    For now, we just check WIN32OLE_EVENT.message_loop is defined.

--- library/win32ole/win32ole_event/message_loop_spec.rb
+++ library/win32ole/win32ole_event/message_loop_spec.rb
@@ -3,7 +3,9 @@ platform_is :windows do
   
   # not sure how we should test this
   describe 'WIN32OLE_EVENT.message_loop' do
-    
+    it 'exists' do
+      lambda { WIN32OLE_EVENT.message_loop }.should_not raise_error NoMethodError
+    end
   end
 
 end
