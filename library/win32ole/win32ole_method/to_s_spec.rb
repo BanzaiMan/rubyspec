66e325ffbc9da94bdb6007677278e0ec49c618fd
Hiro Asari <asari.ruby@gmail.com>
2010-07-19 15:45:21 -0400

    WIN32OLE_METHOD#to_s as an alias to WIN32OLE_METHOD#name.

--- library/win32ole/win32ole_method/to_s_spec.rb
+++ library/win32ole/win32ole_method/to_s_spec.rb
@@ -4,7 +4,7 @@ platform_is :windows do
   require 'win32ole'
   
   describe 'WIN32OLE_METHOD#name' do
-    it_behaves_like :win32ole_method_name, :name
+    it_behaves_like :win32ole_method_name, :to_s
     
   end
   
